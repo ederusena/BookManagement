f96b4817e421ec99699ebb04207916313613288f
Eder Sena <eder.sena@live.com>
2024-08-18 11:45:09 -0300

    chore: update menu option for loan in Program.cs

--- Program.cs
+++ Program.cs
@@ -31,7 +31,7 @@ namespace BookManagement
         Console.ForegroundColor = ConsoleColor.Cyan;
         Console.WriteLine("1. 📚 Menu Livro");
         Console.WriteLine("2. 🙋 Menu Usuário");
-        Console.WriteLine("2. 🏦 Menu Empréstimo");
+        Console.WriteLine("3. 🏦 Menu Empréstimo");
         Console.ForegroundColor = ConsoleColor.Red;
         Console.WriteLine("5. 🔚 Sair");
       }
